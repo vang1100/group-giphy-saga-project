6639cf54c3ace28ae2709e50a72113323a6457a3
Padee Vang <vangpadee11@gmail.com>
2024-09-24 15:33:59 -0500

    planned out some details...

---
+++ planning.md
@@ -0,0 +1,14 @@
+1. Search view
+- input field to enter search string
+- a button to submit the search
+- GET from api
+- display results
+- a like button or onclick to favorite a image
+- post like to data
+
+
+2. View
+- display the favorited images
+    - useSelector
+- set cateogray for each image
+    - trigger the databse?
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,14 @@ function App() {
   return (
     <div>
       <h1>Giphy Search!</h1>
+
+      <h2>Search View</h2>
+      <input></input>
+      <button type="submit">Search</button>
+
+      <h2>Favorites View</h2>
+      <li>Images will display here</li>
+      <button>Like</button> This button will do a "post" to the database for favorites
     </div>
   );
 }
